768fe38aad436655e40a9f57921bd4d0b99bb869
John Tang -X (jotang2 - SOFTCLOUDS LLC at Cisco) <jotang2@cisco.com>
2022-01-24 13:50:48 -0800

    Change sleep to 2 seconds and add exclamation point to the System.out.println

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -4,9 +4,9 @@ public class WorkerProcess
     {
         while(true) {
         	try { 
-        	    Thread.sleep(1000);
+        	    Thread.sleep(2000);
             } catch(InterruptedException e) {}
-            System.out.println("Worker process woke up");
+            System.out.println("Worker process woke up!");
         }
     }    
 }
